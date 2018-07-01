ea4ebc34f988ca4d2d29b4d47e84e3a67cdd9d72
郑海青 <zhenghq@macair.local>
2018-07-01 12:40:20 +0800

    实现kmp

--- ds/KMP.py
+++ ds/KMP.py
@@ -47,4 +47,52 @@ def find(string,pattern):
     return -1
 
 
-print(find(sys.argv[1],sys.argv[2]))
+
+def nextArray(str):
+    if str == None or len(str) == 0:
+        return None
+
+    if len(str) ==1:
+        return [-1]
+    if len(str) == 2:
+        return [-1,0]
+
+    next = [0] * len(str)
+    next[0] = -1
+    i = 0
+    j = 2
+    while j<len(str):
+        if i==-1 or str[j-1] == str[i]:
+            i=i+1
+            next[j]=i
+            j=j+1
+        else:
+            i=next[i]
+    return next
+
+def index(str,sub):
+    if sub==None or len(sub) == 0:
+        return -1
+
+    next = nextArray(sub)
+
+    i = 0
+    j = 0
+
+    while  i < len(str) and j < len(sub):
+        if str[i] == sub[j] or j==-1:
+            i = i+1
+            j=j+1
+        else:
+            j=next[j]
+    #只有遍历完所有的字串才能确定是否包含完整的子串
+    if j == len(sub):
+        return i-j
+    return -1
+
+
+
+print(nextArray('aasbgedddad'))
+print(next('aasbgedddad'))
+print(nextArray('aaaa'))
+print(nextArray('fjalifiilllfjajiifjad'))
